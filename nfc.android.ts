708a1a9b91ef5b3fce2597f46f3a1646f615548a
Eddy Verbruggen <eddyverbruggen@gmail.com>
2017-04-10 11:05:22 +0200

    bump

--- nfc.android.ts
+++ nfc.android.ts
@@ -248,31 +248,31 @@ class Activity extends android.app.Activity {
   }
 
   protected onSaveInstanceState(outState: android.os.Bundle): void {
-      this._callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
+    this._callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
   }
 
   protected onStart(): void {
-      this._callbacks.onStart(this, super.onStart);
+    this._callbacks.onStart(this, super.onStart);
   }
 
   protected onStop(): void {
-      this._callbacks.onStop(this, super.onStop);
+    this._callbacks.onStop(this, super.onStop);
   }
 
   protected onDestroy(): void {
-      this._callbacks.onDestroy(this, super.onDestroy);
+    this._callbacks.onDestroy(this, super.onDestroy);
   }
 
   public onBackPressed(): void {
-      this._callbacks.onBackPressed(this, super.onBackPressed);
+    this._callbacks.onBackPressed(this, super.onBackPressed);
   }
 
   public onRequestPermissionsResult(requestCode: number, permissions: Array<String>, grantResults: Array<number>): void {
-      this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, undefined /*TODO: Enable if needed*/);
+    this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, undefined /*TODO: Enable if needed*/);
   }
 
   protected onActivityResult(requestCode: number, resultCode: number, data: android.content.Intent): void {
-      this._callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
+    this._callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
   }
 
   onNewIntent(intent: android.content.Intent): void {
